f8b4c0024c53be7e81b2b77eff6fc781377433ab
Lesley <lkompare87@gmail.com>
2015-09-15 20:57:04 -0500

    Clean up the importer class

--- lib/importer.rb
+++ lib/importer.rb
@@ -3,24 +3,14 @@ require_relative "../app/models/company"
 
 class ImportData
 
-
-	attr_accessor :file, :unquoted
+	attr_accessor :file
 
 	def initialize(file)
 		@file = file
-		@unquoted = remove_quotes
-	end
-
-
-	
-	def remove_quotes
-		quoted = File.read(@file)
-		@unquoted = quoted.gsub("\"", "")
 	end
 
 
 	def import
-
 		counter = 0
 	 	CSV.foreach(@file, headers: true) do |row|
 	 		row_hash = row.to_hash
@@ -32,7 +22,6 @@ class ImportData
 	 		company.save!
 	 		counter += 1 if company.persisted? 
 	    end
-
 	    p "Imported #{counter} companies"
 	end
 
